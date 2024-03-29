b3d58d1a7716c3a8b70b7d7f8a3d297683fc7aba
AliaksandraW <aleks.vasilevich@gmail.com>
2024-03-29 17:27:27 -0400

    finra by Sasha

---
+++ src/aliaksandra_wasilewic/week1/FINRA.java
@@ -0,0 +1,10 @@
+package aliaksandra_wasilewic.week1;
+
+public class FINRA {
+    public static void main(String[] args) {
+        int a = 15;
+        if(a%5==0){
+            System.out.println();
+        }
+    }
+}
