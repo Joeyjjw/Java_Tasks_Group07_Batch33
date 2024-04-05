7cdee7055f61b6ed1363c4be9a92a7ad52bc3fd1
AliaksandraW <aleks.vasilevich@gmail.com>
2024-04-05 18:51:57 -0400

    EvenOrOdd and Finra with Scanner

--- src/aliaksandra_wasilewic/week1/FINRA.java
+++ src/aliaksandra_wasilewic/week1/FINRA.java
@@ -1,10 +1,29 @@
 package aliaksandra_wasilewic.week1;
 
+import java.util.Scanner;
+
 public class FINRA {
+    /** 3. Create a class called FINRA. Write a function that prints out the number.
+     For a number that is a multiple of 3, print "FIN" instead of the number.
+     For a number that is a multiple of 5, print "RA" instead of the number.
+     For a number that is a multiple of both 3 and 5, print "FINRA" instead of the number.
+     Example:
+     number = 15
+     Output:
+     FINRA
+     **/
     public static void main(String[] args) {
-        int a = 15;
-        if(a%5==0){
-            System.out.println();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please, write your number");
+        int num = input.nextInt();
+        if(num%3==0) {
+            System.out.println("FIN");
+        }else if(num%5==0){
+            System.out.println("RA");
+        }else if (num%3==0 && num%5==0){
+            System.out.println("FINRA");
+        }else {
+            System.out.println("Result of dividing by 3 or 5 not an integer. Number isn't FIN-RA");
         }
     }
 }
--- src/aliaksandra_wasilewic/week1/OddOrEven.java
+++ src/aliaksandra_wasilewic/week1/OddOrEven.java
@@ -1,8 +1,18 @@
 package aliaksandra_wasilewic.week1;
 
+import java.util.Scanner;
+
 public class OddOrEven {
     public static void main(String[] args) {
-        int z = 15;
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please, enter whole number");
+        int num = input.nextInt();
+        if (num%2==0){
+            System.out.println("Your number is even");
+        }else if(num%2!=0){
+            System.out.println("Your number is odd");
+        }else {
+            System.out.println("Your number is incorrect");
+        }
     }
 }
